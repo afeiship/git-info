300fe3a3bdb20ae8cce218104e1f6817d53de389
afeiship <1290657123@qq.com>
2017-06-26 11:32:54 +0800

    bugfix for current Branch

--- src/git-info.js
+++ src/git-info.js
@@ -1,17 +1,21 @@
 (function () {
 
   var exec = require('child_process').execSync;
+  var spaceRE = /[\s\r\n]+$/;
   var EMPTY_STR = '';
+
+
+
   var removeEmpty = function (inString) {
     return inString.replace(/[\s\r\n]+$/, EMPTY_STR);
   };
 
   function branch() {
-    return removeEmpty(EMPTY_STR+exec('git branch'));
+    return removeEmpty(EMPTY_STR + exec('git branch'));
   }
 
   function currentBranch(){
-    return removeEmpty(EMPTY_STR+exec('git branch | grep \*'));
+    return removeEmpty(EMPTY_STR + exec('git branch | grep \\\*'));
   }
 
   module.exports = {
