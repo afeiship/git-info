a8d728aa432093cb401e78ede92a630d9d6b4eb3
afeiship <1290657123@qq.com>
2017-06-27 22:25:49 +0800

    update cmd

--- src/git-info.js
+++ src/git-info.js
@@ -5,21 +5,24 @@
   var EMPTY_STR = '';
 
 
-
   var removeEmpty = function (inString) {
     return inString.replace(spaceRE, EMPTY_STR);
   };
 
+  var execCmd = function(inCmd){
+    return removeEmpty( EMPTY_STR + exec(inCmd) );
+  };
+
   function branch() {
-    return removeEmpty(EMPTY_STR + exec('git branch'));
+    return execCmd('git branch');
   }
 
   function currentBranch(){
-    return removeEmpty(EMPTY_STR + exec('git rev-parse --abbrev-ref HEAD'));
+    return execCmd('git rev-parse --abbrev-ref HEAD');
   }
 
   function hash(){
-    return removeEmpty( EMPTY_STR + exec('git rev-parse --verify HEAD'))
+    return execCmd('git rev-parse --verify HEAD');
   }
 
   module.exports = {
